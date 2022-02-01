193269d9015ddc3f1f43b22c076d8ab5f7610deb
Renato Veneroso <rveneroso@gmail.com>
2022-02-01 10:56:55 -0300

    Importação e uso da classe CPF da biblioteca validate-docbr.

--- Cpf.py
+++ Cpf.py
@@ -1,24 +1,25 @@
+from validate_docbr import CPF
+
 class Cpf:
     def __init__(self, documento):
         documento = str(documento)
         if(self.cpf_eh_valido(documento)):
             self.cpf = documento
         else:
-            raise ValueError("CPF inválido")
+            raise ValueError("CPF inválido!!")
 
     def __str__(self):
         return self.format_cpf()
 
     def cpf_eh_valido(self, documento):
         if(len(documento)==11):
-            return True
+            validador = CPF()
+            return validador.validate(documento)
         else:
-            return False
+            raise ValueError("Quantidade de dígitos inválida!!")
 
     def format_cpf(self):
-        grupo1 = self.cpf[:3]
-        grupo2 = self.cpf[3:6]
-        grupo3 = self.cpf[6:9]
-        grupo4 = self.cpf[9:]
-        return "{}.{}.{}-{}".format(grupo1, grupo2, grupo3, grupo4)
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Cpf import Cpf
-
 cpf = 66826858070
 objeto_cpf = Cpf(cpf)
 print(objeto_cpf)
+print(objeto_cpf.cpf_eh_valido(objeto_cpf.cpf))
 
 

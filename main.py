c5cc02a7b618d2de7751411078bacbb81cc24c0d
Renato Veneroso <rveneroso@gmail.com>
2022-02-01 15:18:33 -0300

    Adição do arquivo telefones_br contendo uma classe responsável por criar e validar números de telefones.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from cpf_cnpj import Documento
-cpf = "66826858070"
-objeto_cpf = Documento.cria_documento(cpf)
-print(objeto_cpf)
+from telefones_br import TelefonesBr
+# cpf = "66826858070"
+# objeto_cpf = Documento.cria_documento(cpf)
+# print(objeto_cpf)
+#
+# cnpj = "31117285000179"
+# objeto_cnpj = Documento.cria_documento(cnpj)
+# print(objeto_cnpj)
 
-cnpj = "31117285000179"
-objeto_cnpj = Documento.cria_documento(cnpj)
-print(objeto_cnpj)
-
-
+numero = '2126451234'
+telefone = TelefonesBr(numero)
 
---
+++ telefones_br.py
@@ -0,0 +1,16 @@
+import re
+
+class TelefonesBr:
+    def __init__(self, telefone):
+        if(self.valida_telefone(telefone)):
+            self.numero = telefone
+        else:
+            raise ValueError("Número de telefone inválido!!")
+
+    def valida_telefone(self, telefone):
+        padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+        resposta = re.findall(padrao, telefone)
+        if(resposta):
+            return True
+        else:
+            return False

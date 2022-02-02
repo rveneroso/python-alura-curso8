a81f76b72fae4ac847ad1063f46fa9790aca7cff
Renato Veneroso <rveneroso@gmail.com>
2022-02-01 22:08:12 -0300

    Inclusão da classe responsável por validar e formatar CEPs.

---
+++ acesso_cep.py
@@ -0,0 +1,20 @@
+class BuscaEndereco:
+
+    def __init__(self, cep):
+        cep = str(cep)
+        if(self.cep_e_valido(cep)):
+            self.cep = cep
+        else:
+            raise ValueError("CEP inválido!!")
+
+    def __str__(self):
+        return self.formata_cep()
+
+    def cep_e_valido(self, cep):
+        if(len(cep)==8):
+            return True
+        else:
+            return False
+
+    def formata_cep(self):
+        return "{}-{}".format(self.cep[:5], self.cep[5:])
--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from datetime import datetime, timedelta
-from datas_br import DatasBr
-# print(datetime.today())
-#
-# cadastro = DatasBr()
-# print(cadastro.dia_semana_cadastro())
+from acesso_cep import BuscaEndereco
 
-hoje = datetime.today()
-# formato_data = "%d/%m/%Y %H:%m"
-# print(hoje.strftime(formato_data))
-hoje = DatasBr()
-print(hoje.tempo_cadastro())
+cep = 30510550
+objeto_cep = BuscaEndereco(cep)
+print(objeto_cep)
